ac5d088dad8fcb083fccb77c22eb6690a0ff1c9d
Vrunda01 <vsarvaiya088@rku.ac.in>
2020-10-19 16:01:35 +0530

    Initial commit

---
+++ app/src/main/java/com/rku/tutorial12/MainActivity.java
@@ -0,0 +1,75 @@
+package com.rku.tutorial12;
+
+import android.os.Bundle;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView rcvUsers;
+    RequestQueue requestQueue;
+    JsonArrayRequest jsonArrayRequest;
+    UserAdapter userAdapter;
+    LinearLayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rcvUsers = findViewById(R.id.rcvUsers);
+        rcvUsers.setHasFixedSize(true);
+
+        //use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        rcvUsers.setLayoutManager(layoutManager);
+
+        //Add Divider
+        DividerItemDecoration dividerItemDecoration =
+                new DividerItemDecoration(rcvUsers.getContext(),LinearLayoutManager.VERTICAL);
+        rcvUsers.addItemDecoration(dividerItemDecoration);
+
+        //Apply Animation
+        int resId = R.anim.layout_animation_fall_down;
+        LayoutAnimationController animation = AnimationUtils.loadLayoutAnimation(getApplicationContext(), resId);
+        rcvUsers.setLayoutAnimation(animation);
+
+        volleyNetworkCallAPI();
+    }
+
+    private void volleyNetworkCallAPI() {
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                Util.URL_USER,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Util.userdata = response;
+                        userAdapter = new UserAdapter(response);
+                        rcvUsers.setAdapter(userAdapter);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+        requestQueue.add(jsonArrayRequest);
+    }
+}
